ca6e57bfed381e678547e6b2f288176b29551dd3
dumitruai <dantohi2012@gmail.com>
2022-06-17 00:00:01 +0300

    Fierst

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchBarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
